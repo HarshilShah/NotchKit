2cdad0cf816fc9052d8fdb24ea2bf80a49b52a2d
harshilshah <harshilshah1910@outlook.com>
2017-07-18 19:58:48 +0530

    Just setting up my Notchless

---
+++ Example/Sources/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotchlessExample
+//
+//  Created by Harshil Shah on 16/09/17.
+//  Copyright © 2017 Harshil Shah. All rights reserved.
+//
+
+import UIKit
+import Notchless
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
