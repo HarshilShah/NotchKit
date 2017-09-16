ea3a409582fde4030b2c850ee36304625261f13f
harshilshah <harshilshah1910@outlook.com>
2017-09-16 19:14:47 +0530

    Added a bit of content to the example app

--- Example/Sources/ViewController.swift
+++ Example/Sources/ViewController.swift
@@ -10,10 +10,30 @@ import UIKit
 import NotchKit
 
 class ViewController: UIViewController {
+    
+    private let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        
+        label.text = "[Insert your app here]"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = UIFont(name: "Menlo-Bold", size: 40)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 32),
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -32)
+        ])
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
 }
