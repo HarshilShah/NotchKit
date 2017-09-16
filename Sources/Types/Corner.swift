d30fc887fcea7ec1f372e8f918f5b08e60fa9140
harshilshah <harshilshah1910@outlook.com>
2017-09-16 15:11:26 +0530

    Added a `Corner` type for use throughout the framework

---
+++ Sources/Types/Corner.swift
@@ -0,0 +1,15 @@
+//
+//  Corner.swift
+//  Notchless
+//
+//  Created by Harshil Shah on 16/09/17.
+//  Copyright © 2017 Harshil Shah. All rights reserved.
+//
+
+/// A simple enum used to refer to a `CGRect`'s corner
+enum Corner {
+    case topLeft
+    case topRight
+    case bottomRight
+    case bottomLeft
+}
