753ecc3939ab1a2f9577e352c7eb683fbdd398a0
harshilshah <harshilshah1910@outlook.com>
2017-09-18 14:01:29 +0530

    Fixes a crash on relaunch

--- Example/Sources/AppDelegate.swift
+++ Example/Sources/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = NotchKitWindow(frame: UIScreen.main.bounds)
         let rootViewController = ViewController()
--- Example/Sources/ViewController.swift
+++ Example/Sources/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import NotchKit
 
 class ViewController: UIViewController {
     
